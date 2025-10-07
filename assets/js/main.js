1cfab95ebbf621aed325af2606e096fc45b95877
Yevhen Onoprienko <fromzpcity@gmail.com>
2025-10-07 18:02:06 +0300

    03. usefull-things-204-text-skew. Bg, color animation.

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,8 +1,11 @@
 document.addEventListener("DOMContentLoaded", () => {
-    const box = document.querySelector(".text-box");
     const textContainer = document.querySelector(".text h1");
 
     textContainer.innerHTML = textContainer.textContent.replace(/\S/g, "<span class='letter'>$&</span>");
 
     const spans = textContainer.querySelectorAll("span");
+
+    spans.forEach((span, i) => {
+        span.style.setProperty("--i", i);
+    });
 });
